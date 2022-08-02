17dc0b5268c2f20817ab83ca3501c21a35cd99e4
Elias Gonzalez <48927897+red-peel@users.noreply.github.com>
2022-08-02 00:46:05 -0500

    added test code to main to test book class.

--- Main.java
+++ Main.java
@@ -2,6 +2,16 @@
 import java.util.*;
 class Main {
   public static void main(String[] args) {
+    //testing book object
     
-  }
+    Book sheesh = new Book("Things Fall Apart", "Fiction", "Historical","Chinua Achebe",false, 298);
+    Book sheesh2 = new Book("1", "2", "3", "4", true);
+    
+    System.out.println(sheesh.getInfo());
+    System.out.println(sheesh2.getInfo());
+    
+
+    sheesh.setIsAvailable(true);
+
+  }  
 }
