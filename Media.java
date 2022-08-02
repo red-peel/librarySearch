f18142063faaa9cf8db0ee8eaf8412f17a3fc7c3
Elias Gonzalez <48927897+red-peel@users.noreply.github.com>
2022-08-01 22:10:25 -0500

    Update Media.java

--- Media.java
+++ Media.java
@@ -3,18 +3,38 @@ abstract class media{
     protected String type;
     protected String genre;
     protected String author;
-
+    
+    //setters (return nothing, print item)
     void setTitle(String title){
+        this.title = title;
         System.out.println("The title is set to " + this.title);
     }
     void setType(String type){
+        this.type = type;
         System.out.println("The type is set to " + this.type);
     }
     void setGenre(String genre){
+        this.genre = genre;
         System.out.println("The genre is set to " + this.genre);
     }
     void setAuthor(String author){
+        this.author = author;
         System.out.println("The author is set to " + this.author);
     }
     
+    //getters (return strings)
+    protected String getTitle(){
+        return title;
+    }
+    protected String getType(){
+        return type;
+    }
+    protected String getGenre(){
+        return genre;
+    }
+    protected String getAuthor() {
+        return author;
+    }
+
+    abstract String getInfo();
 }
