ccee8a05e0287d4f48f7518b0aff8fb29eda2441
Elias Gonzalez <48927897+red-peel@users.noreply.github.com>
2022-08-01 21:48:28 -0500

    implemented basic media superclass

--- Media.java
+++ Media.java
@@ -1,1 +1,20 @@
-class media
+abstract class media{
+    protected String title;
+    protected String type;
+    protected String genre;
+    protected String author;
+
+    void setTitle(String title){
+        System.out.println("The title is set to " + this.title);
+    }
+    void setType(String type){
+        System.out.println("The type is set to " + this.type);
+    }
+    void setGenre(String genre){
+        System.out.println("The genre is set to " + this.genre);
+    }
+    void setAuthor(String author){
+        System.out.println("The author is set to " + this.author);
+    }
+    
+}
