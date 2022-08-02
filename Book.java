b8d2054434ed2d65a7b6c200f2df16c0f09d70bf
Elias Gonzalez <48927897+red-peel@users.noreply.github.com>
2022-08-02 00:45:53 -0500

    Finalized book class.

--- Book.java
+++ Book.java
@@ -1,6 +1,30 @@
-public class Book extends Media{
-    public Book(String title, String type, String genre, String author, int pageCount){
-        super(title, type, genre, author);
-        System.out.println("Media: " + title + ". has been created");
+public class Book extends Media {
+    private int pageCount;
+    private boolean isPageCountEnabled;
+
+    // CONSTRUCTOR WITH NO PAGE NUMBER
+    public Book(String title, String type, String genre, String author,boolean isAvailable) {
+        super(title, type, genre, author,isAvailable);
+        isPageCountEnabled = false;
+        // UNCOMMENT BELOW TO SEE WHEN OBJECT HAS BEEN CREATED
+        // System.out.println("Book: " + title + ". has been created");
+    }
+
+    // CONSTRUCTOR WITH PAGE NUMBER
+    public Book(String title, String type, String genre, String author, boolean isAvailable, int pageCount) {
+        super(title, type, genre, author, isAvailable);
+        this.pageCount = pageCount;
+        isPageCountEnabled = true;
+        // UNCOMMENT BELOW TO SEE WHEN OBJECT HAS BEEN CREATED
+        // System.out.println("Book: " + title + ". has been created with: " + this.pageCount + " pages");
+    }
+
+    @Override
+    public String getInfo() {
+        if (isPageCountEnabled) {
+            return this.toString() + " is a " + this.type + " book with: " + this.pageCount + " pages. Its genre: " + this.genre + "and it is "+ this.availibilityCheck();
+        } else {
+            return this.toString() + " is a " + this.type + ". Its genre is " + this.genre + " and it is "+ this.availibilityCheck();
+        }
     }
 }
